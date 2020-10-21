2ab658ea1704436308719cc59677790830349010
Gabriela Campos <gabbycampos31@gmail.com>
2020-10-21 18:19:40 -0500

    bootstrap

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask, request, render_template, redirect, session, jsonify
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretKey'
 
 boggle_game = Boggle()
 
 @app.route('/')
 def home():
   board = boggle_game.make_board()
+  session['board'] = board
   return render_template('index.html', board=board)
 
 
