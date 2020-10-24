011808eae30639c6510ae2e799ce11e45980286a
Gabriela Campos <gabbycampos31@gmail.com>
2020-10-24 11:48:46 -0500

    added debug toolbar

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from boggle import Boggle
 from flask import Flask, request, render_template, redirect, session, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretKey'
+
+debug = DebugToolbarExtension(app)
 
 # instance of class Boggle
 boggle_game = Boggle()
@@ -19,9 +22,9 @@ def home():
 def submit_guess():
   """ receives word from user, checks if valid, returns json response """
 
-  word_guess = request.args['word']
+  word = request.args['word']
   board = session['board']
-  res = boggle_game.check_valid_word(board, word_guess)
+  res = boggle_game.check_valid_word(board, word)
 
   return jsonify(({'result': res}))
 
--- static/app.js
+++ static/app.js
@@ -12,7 +12,6 @@ $('#word-form').submit(function(event) {
   let query = $('#word-submit').val()
   checkWord(query)
   //console.log(query)
-  $('form: input').val('');
 
 });
 
@@ -20,19 +19,19 @@ let score = 0;
 let wordsArr = [];
 // sends word to server to check if it's valid, post on front-end
 async function checkWord(query) {
-  let word_guess = query
-  const response = await axios.get('http://127.0.0.1.5000/submit-guess', {params: {word: word_guess}})
+  //let word_guess = query
+  const response = await axios.get('http://127.0.0.1.5000/submit-guess', {params: {word: query}})
   if (response.data.result === 'not-word') {
-    $('#message').html(`${word_guess} is not a valid word!`);
+    $('#message').html(`${query} is not a valid word!`);
   }
   else if (response.data.result === 'not-on-board') {
-    $('#message').html(`${word_guess} is not a valid word on this board!`);
+    $('#message').html(`${query} is not a valid word on this board!`);
   }
   else {
     $('#score').html(score)
-    $('#your-words').append(`<li>${word_guess}</li>`)
-    wordsArr.push(`${word_guess}`)
-    $('#message').html(`Added: ${word_guess}!`);
+    $('#your-words').append(`<li>${query}</li>`)
+    wordsArr.push(`${query}`)
+    $('#message').html(`Added: ${query}!`);
   }
 }
 
